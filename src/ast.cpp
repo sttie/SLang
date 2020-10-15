4e8c25b5d5fa980fdee352129dda04b8816e71d0
sttie <sttiemath@gmail.com>
2020-10-15 15:12:08 +0300

    Refactor some static initializations

--- src/ast.cpp
+++ src/ast.cpp
@@ -4,6 +4,16 @@
 
 using namespace std;
 
+ASTNode::SymTablePtr ASTNode::symtable = MakeNewSymTable();
+std::vector<ASTNode::SymTablePtr> ASTNode::symtable_func_stack = std::vector<ASTNode::SymTablePtr>();
+ASTNode::FuncTable ASTNode::func_table = ASTNode::FuncTable();
+ASTNode::ArgumentsStack ASTNode::func_stack = ASTNode::ArgumentsStack();
+size_t ASTNode::left_argument = 0;
+size_t ASTNode::right_argument = 0;
+vector<size_t> ASTNode::call_stack = vector<size_t>();
+
+
+
 ASTNode::ASTNode(size_t lineno_)
     : lineno(lineno_)
 {
--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,6 @@
 using namespace std;
 
 
-ASTNode::SymTablePtr ASTNode::symtable = MakeNewSymTable();
-std::vector<ASTNode::SymTablePtr> ASTNode::symtable_func_stack = std::vector<ASTNode::SymTablePtr>();
-ASTNode::FuncTable ASTNode::func_table = ASTNode::FuncTable();
-ASTNode::ArgumentsStack ASTNode::func_stack = ASTNode::ArgumentsStack();
-size_t ASTNode::left_argument = 0;
-size_t ASTNode::right_argument = 0;
-vector<size_t> ASTNode::call_stack = vector<size_t>();
-
-
 void EvaluateProgram(list<unique_ptr<ASTNode>>& stmts)
 {
     for (unique_ptr<ASTNode>& node : stmts) {
